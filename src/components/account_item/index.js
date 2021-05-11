dff4c3bab66c2363a9bd55663e74ecf10c0cc26f
Paulo <paulo.rmartins@gmail.com>
2021-05-11 16:28:05 +0100

    displaynetwork specifc address

--- src/components/account_item/index.js
+++ src/components/account_item/index.js
@@ -3,16 +3,34 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
 import { get } from '../../actions/validator'
 import { selectors } from '../../selectors'
+import { encodeAddress } from '@polkadot/util-crypto'
 import ListItem from '@material-ui/core/ListItem';
-// import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Identicon from '@polkadot/react-identicon';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles'
 
+const NETWORK = !!process.env.REACT_APP_NETWORK ? process.env.REACT_APP_NETWORK : 'westend'
+
+const network_prefixes = {
+  'polkadot': 0,
+  'kusama': 2
+}
+
 const stashDisplay = (stash) => {
-	return `${stash.slice(0, 6)}...${stash.slice(stash.length-6, stash.length)}`
+  return `${stash.slice(0, 6)}...${stash.slice(stash.length-6, stash.length)}`
+}
+
+const nameDisplay = (name) => {
+	return name.length > 24 ? `${name.slice(0, 24)}...` : name
+}
+
+const networkDisplay = (stash) => {
+  if (NETWORK in network_prefixes) {
+    return encodeAddress(stash, network_prefixes[NETWORK])
+  }
+  return stash
 }
 
 class AccountItem extends Component {
@@ -30,20 +48,22 @@ class AccountItem extends Component {
 
  	render() {
 		const { classes, id, account } = this.props;
+
+    const stash = networkDisplay(id)
 		
 		return (
-      <ListItem key={id}
+      <ListItem key={stash}
             classes={{
               root: classes.listItemRoot,
             }}
 			>
 				<ListItemAvatar>
 				<Identicon
-          value={id}
+          value={stash}
           size={50}
           theme={'polkadot'} />
         </ListItemAvatar>
-        <ListItemText primary={!!account.name ? account.name : stashDisplay(id)} />
+        <ListItemText primary={!!account.name ? nameDisplay(account.name) : stashDisplay(stash)} />
       </ListItem>
     )
 	}
