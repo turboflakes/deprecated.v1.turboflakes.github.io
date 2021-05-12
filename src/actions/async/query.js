3b2ba9447aaa70e55222c78c953f5868e30e1180
Paulo <paulo.rmartins@gmail.com>
2021-05-12 12:08:32 +0100

    set expiration on fetch requests

--- src/actions/async/query.js
+++ src/actions/async/query.js
@@ -7,7 +7,7 @@ import {
 import serialize from '../../utils/serialize'
 
 
-const isExpired = (state, queryParams, schema, after = 30) => {
+const isExpired = (state, queryParams, schema, after = 60) => {
   const queryKey = serialize(queryParams)
   if (state.collections &&
     state.collections[`${schema.toLowerCase()}`] &&
@@ -21,7 +21,8 @@ const isExpired = (state, queryParams, schema, after = 30) => {
 export const query = (queryParams, schema, headers, force = false, options = {}) => (dispatch, getState) => {
 
   const state = getState()
-  // the same request can only be made after 30s unless force is true
+  
+  // The same request can only be made after X seconds - default is 60s
   if (!force && !isExpired(state, queryParams, schema, options.expire)) {
     return null
   }
