c962cb22efc9a748ded4e25588f6e865b2c6961e
Paulo <paulo.rmartins@gmail.com>
2021-05-27 11:08:15 +0100

    scroll to top

--- src/components/pages/disclaimer_page/index.js
+++ src/components/pages/disclaimer_page/index.js
@@ -8,23 +8,31 @@ import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles'
 
-class AboutPage extends Component {
+class DisclaimerPage extends Component {
+
+  rootRef = React.createRef();
+
+  componentDidMount() {
+    if (this.rootRef.current) {
+      this.rootRef.current.scrollIntoView()
+    }
+  }
 
   render() {
     const { classes } = this.props;
     
     return (
-      <Container className={classes.root}>
+      <Container className={classes.root} ref={this.rootRef}>
         <IconButton aria-label="Back" color="primary" href="/#/" className={classes.backIcon}>
           <ArrowBackIcon />
        </IconButton>
        <Typography variant="h5" color="inherit" align="center" className={classes.header} paragraph>
           Disclaimer
        </Typography>
-       <Typography variant="body1" color="inherit" align="justify" paragraph>
+       <Typography color="inherit" align="justify" paragraph>
        It is very important to do your own research before nominating a validator since there is a risk of <Link href="https://wiki.polkadot.network/docs/en/learn-staking#slashing" target="_blank" rel="noreferrer" color="inherit" underline="always">slashing</Link>.
        </Typography>
-       <Typography variant="body1" color="inherit" align="justify" paragraph>
+       <Typography color="inherit" align="justify" paragraph>
        Turboflakes does not control, take responsibility for, or assume liability for any validator misbehaviour displayed on this Website. Turboflakes only provides a decision support tool that can help you choose among a set of validators based on their traits and your personal preferences.
        </Typography>
       </Container>
@@ -32,8 +40,8 @@ class AboutPage extends Component {
   }
 }
 
-AboutPage.propTypes = {
+DisclaimerPage.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(AboutPage);
+export default withStyles(styles)(DisclaimerPage);
