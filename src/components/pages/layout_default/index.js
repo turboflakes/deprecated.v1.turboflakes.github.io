8a61495f24cd8975084846678d60b21d15321f90
Paulo <paulo.rmartins@gmail.com>
2021-12-09 09:26:16 +0000

    fix go back in default pages

--- src/components/pages/layout_default/index.js
+++ src/components/pages/layout_default/index.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBackRounded';
 import Footer from '../../footer'
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles'
 
 class LayoutDefault extends Component {
   
+  handleBack = () => {
+    const {history} = this.props
+    history.goBack()
+  }
+
   render() {
     const { classes } = this.props;
     
     return (
       <div className={classes.root}>
+        <div className={classes.closeButtonBox}>
+          <IconButton aria-label="Back" classes={{ root: classes.closeButtonRoot }}  
+            onClick={this.handleBack}>
+            <ArrowBackIcon classes={{ root: classes.closeButtonIconRoot }} />
+          </IconButton>
+        </div>
         {this.props.children}
         <Footer />
       </div>
@@ -21,6 +34,5 @@ class LayoutDefault extends Component {
 LayoutDefault.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-
 
 export default withStyles(styles)(LayoutDefault);
--- src/components/pages/layout_default/styles.js
+++ src/components/pages/layout_default/styles.js
@@ -3,5 +3,16 @@ const styles = (theme) => ({
     backgroundColor: "rgba(241, 241, 240, 1)",
     margin: `0 auto`,
   },
+  closeButtonBox:{
+    position: "absolute",
+    right: theme.spacing(3),
+    top: theme.spacing(2)
+  },
+  closeButtonRoot:{
+    color: "#000"
+  },
+  closeButtonIconRoot:{
+    fontSize: 34
+  },
 })
 export default styles
